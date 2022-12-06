d1048c87ab06b5467c74df498d1bd687697a0094
Vadim Tokar <tokar.web@biganto.com>
2022-12-06 21:07:11 +0300

    add declension and location to share

---
+++ packages/common/src/utils/declension/index.ts
@@ -0,0 +1,9 @@
+export const declension = (count: number, variants: string[]) =>
+  variants[
+    ((count %= 100), 20 > count && count > 4)
+      ? 2
+      : [2, 0, 1, 1, 1, 2][((count %= 10), count < 5) ? count : 5]
+  ];
+
+export const BOOK_DECLENSIONS = ["книга", "книги", "книг"];
+export const getBookDeclensions = (count: number) => declension(count, BOOK_DECLENSIONS);
