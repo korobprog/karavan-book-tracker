4cf6bb4ed7e94ebed4d231f1cedd5176200ef5e0
Andrei.Surkov@mkcorp.com <Andrei.Surkov@mkcorp.com>
2023-02-17 20:34:12 +0300

    code review fix

--- packages/common/src/components/TeamCard.tsx
+++ packages/common/src/components/TeamCard.tsx
@@ -25,7 +25,7 @@ import {
 import { LocationDoc } from "../services/api/locations";
 import { HashMap } from "../utils/getHashMap";
 import { setUserTeam, TeamMemberStatus, UserDocWithId } from "../services/api/useUser";
-import { UsersStatistic } from "common/src/features/downloadUsersStatistic/index";
+import { UsersStatistic } from "common/src/features/downloadUsersStatistic";
 
 type Props = {
   team: TeamDoc;
