8a8d8d25c59e0b32d6e1f9cf2806e48581e83e0a
Maxim Korobkov <62425861+korobprog@users.noreply.github.com>
2023-09-18 10:23:53 +1000

    inicial donations page (#100)
    * inicial donations page
    
    * avatar dow styles
    
    * fix

--- packages/donation/src/App.tsx
+++ packages/donation/src/App.tsx
@@ -3,13 +3,17 @@ import { Route, Routes } from "react-router-dom";
 
 import "./App.less";
 import { Home } from "./pages/home";
+import { VarnaG } from "./pages/varna-gauranga";
 import { routes } from "././shared/routes";
+import { EvgenyK } from "./pages/evgeny-kovalsky";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path={routes.root} element={<Home />} />
+        <Route path={routes.varnag} element={<VarnaG />} />
+        <Route path={routes.evgenyk} element={<EvgenyK />} />
       </Routes>
     </div>
   );
