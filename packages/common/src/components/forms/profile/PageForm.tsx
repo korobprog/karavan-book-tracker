e04aa517bffd4f092fc1a8640b05d5fd354f0657
Максим Коробков <maksimkorobkov@iMac-Maksim.local>
2023-11-28 21:50:36 +1000

    fix mentor

--- packages/common/src/components/forms/profile/PageForm.tsx
+++ packages/common/src/components/forms/profile/PageForm.tsx
@@ -19,9 +19,10 @@ import telegram from "common/src/images/telegram.svg";
 import whats from "common/src/images/whatsapp.svg";
 import email from "common/src/images/email.svg";
 import { CurrentUser } from "common/src/services/api/useCurrentUser";
-import { routes } from "../../../../../tracker/src/shared/routes";
-import jsPDF from "jspdf";
-import logo from "../image/book-danation.svg";
+import logo from "../../../images/book-danation.svg";
+import printPdfDonations from "./printPdfDonations";
+
+const QR_SIZE = 160;
 
 export type PageFormValues = DonationPageDoc;
 
@@ -37,7 +38,7 @@ export const PageForm = (props: Props) => {
   const { currentUser } = props;
 
   const userId = currentUser.profile?.id || currentUser.user?.uid;
-  const nameuser = currentUser.profile?.name;
+  const userName = currentUser.profile?.name;
 
   const [switchState, setSwitchState] = useState(initialValues.active);
 
@@ -47,7 +48,6 @@ export const PageForm = (props: Props) => {
 
   const { Link } = Typography;
   const myPageLink = `https://books-donation.web.app/page/${userId}`;
-  const [size] = useState<number>(160);
 
   const { Text } = Typography;
 
@@ -71,7 +71,7 @@ export const PageForm = (props: Props) => {
 
       {switchState ? (
         <Space style={{ marginLeft: 30 }}>
-          <Link onClick={PdfPrintDonations} target="_blank">
+          <Link onClick={printPdfDonations} target="_blank">
             распечатать визитки
           </Link>
           <PrinterTwoTone />
@@ -213,22 +213,21 @@ export const PageForm = (props: Props) => {
 
         {switchState ? (
           <div id="myqrcode">
-            <Text>{nameuser} это Ваш QR странички донатов</Text>
+            <Text>{userName} это Ваш QR странички донатов</Text>
             <QRCode
               className="centred"
               value={myPageLink}
               bgColor="#fff"
               style={{ marginBottom: 16 }}
               errorLevel="H"
-              size={size}
-              iconSize={size / 4}
+              size={QR_SIZE}
+              iconSize={QR_SIZE / 4}
               icon={logo}
             />
           </div>
         ) : (
           ""
         )}
-
         <Divider dashed />
         <Form.Item>
           <Button type="primary" htmlType="submit">
@@ -248,63 +247,4 @@ export const PageForm = (props: Props) => {
       </div>
     </Form>
   );
-};
-
-const PdfPrintDonations = () => {
-  let url;
-  const canvas = document.getElementById("myqrcode")?.querySelector<HTMLCanvasElement>("canvas");
-  if (canvas) {
-    url = canvas.toDataURL();
-    const aref = document.createElement("a");
-    aref.href = url;
-    const doc = new jsPDF({
-      format: "a4",
-      unit: "px",
-    });
-    //vertical lines
-    doc.line(335, 1, 335, 800);
-    doc.line(225, 1, 225, 800);
-    doc.line(115, 1, 115, 800);
-    //Horizontal lines
-    doc.line(500, 135, 1, 135);
-    doc.line(500, 270, 1, 270);
-    doc.line(500, 410, 1, 410);
-    //1 first line
-    doc.addImage(canvas, "JPEG", 10, 25, 100, 100);
-    doc.text("Donate to books", 10, 20);
-    doc.addImage(canvas, "JPEG", 120, 25, 100, 100);
-    doc.text("Donate to books", 120, 20);
-    doc.addImage(canvas, "JPEG", 230, 25, 100, 100);
-    doc.text("Donate to books", 230, 20);
-    doc.addImage(canvas, "JPEG", 340, 25, 100, 100);
-    doc.text("Donate to books", 340, 20);
-    //2 first line
-    doc.addImage(canvas, "JPEG", 10, 160, 100, 100);
-    doc.text("Donate to books", 10, 150);
-    doc.addImage(canvas, "JPEG", 120, 160, 100, 100);
-    doc.text("Donate to books", 120, 150);
-    doc.addImage(canvas, "JPEG", 230, 160, 100, 100);
-    doc.text("Donate to books", 230, 150);
-    doc.addImage(canvas, "JPEG", 340, 160, 100, 100);
-    doc.text("Donate to books", 340, 150);
-    //3 first line
-    doc.addImage(canvas, "JPEG", 10, 300, 100, 100);
-    doc.text("Donate to books", 10, 290);
-    doc.addImage(canvas, "JPEG", 120, 300, 100, 100);
-    doc.text("Donate to books", 120, 290);
-    doc.addImage(canvas, "JPEG", 230, 300, 100, 100);
-    doc.text("Donate to books", 230, 290);
-    doc.addImage(canvas, "JPEG", 340, 300, 100, 100);
-    doc.text("Donate to books", 340, 290);
-    //4 first line
-    doc.addImage(canvas, "JPEG", 10, 435, 100, 100);
-    doc.text("Donate to books", 10, 425);
-    doc.addImage(canvas, "JPEG", 120, 435, 100, 100);
-    doc.text("Donate to books", 120, 425);
-    doc.addImage(canvas, "JPEG", 230, 435, 100, 100);
-    doc.text("Donate to books", 230, 425);
-    doc.addImage(canvas, "JPEG", 340, 435, 100, 100);
-    doc.text("Donate to books", 340, 425);
-    doc.save("print.pdf");
-  }
 };
