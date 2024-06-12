a1c00b37b520f1229a337a424b9df1665573ec48
Korobprog <korobprog@gmail.com>
2024-06-12 22:43:07 +1000

    modal

--- packages/common/src/components/LocationSelect.tsx
+++ packages/common/src/components/LocationSelect.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { PlusOutlined } from "@ant-design/icons";
-import { Select, SelectProps, Typography, RefSelectProps } from "antd";
+import { Select, SelectProps, Typography, RefSelectProps, Modal } from "antd";
 
 type LocationSelectProps = SelectProps & {
   onAddNewLocation: () => void;
@@ -14,41 +14,60 @@ export const LocationSelect = React.forwardRef<RefSelectProps, LocationSelectPro
     const { onAddNewLocation, locationSearchString, children, isOnline, loading, ...restProps } =
       props;
 
-    return (
-      <Select
-        ref={ref}
-        showSearch
-        placeholder="Начните вводить..."
-        defaultActiveFirstOption={false}
-        autoClearSearchValue={false}
-        suffixIcon={null}
-        filterOption={false}
-        allowClear
-        notFoundContent={
-          !loading && (
-            <>
-              <Typography.Paragraph style={{ whiteSpace: "nowrap" }}>
-                Такого места пока нет
-              </Typography.Paragraph>
+    const [modal1Open, setModal1Open] = useState(false);
 
-              {isOnline ? (
-                <Typography.Link onClick={onAddNewLocation} style={{ whiteSpace: "nowrap" }}>
-                  <PlusOutlined />
-                  Добавить "{locationSearchString}"
-                </Typography.Link>
-              ) : (
-                <Typography.Text>
-                  Пока вы оффлайн нельзя добавить город, подключите интернет и проверьте, возможно
-                  такой город уже есть в базе
-                </Typography.Text>
-              )}
-            </>
-          )
-        }
-        {...restProps}
-      >
-        {children}
-      </Select>
+    <Typography.Link onClick={onAddNewLocation} style={{ whiteSpace: "nowrap" }}>
+      <PlusOutlined />
+      Добавить "{locationSearchString}"
+    </Typography.Link>;
+
+    return (
+      <>
+        <Select
+          ref={ref}
+          showSearch
+          placeholder="Начните вводить..."
+          defaultActiveFirstOption={false}
+          autoClearSearchValue={false}
+          suffixIcon={null}
+          filterOption={false}
+          allowClear
+          notFoundContent={
+            !loading && (
+              <>
+                <Typography.Paragraph style={{ whiteSpace: "nowrap" }}>
+                  Такого места пока нет
+                </Typography.Paragraph>
+
+                {isOnline ? (
+                  <Typography.Link
+                    onClick={() => setModal1Open(true)}
+                    style={{ whiteSpace: "nowrap" }}
+                  >
+                    <PlusOutlined />
+                    Добавить "{locationSearchString}"
+                  </Typography.Link>
+                ) : (
+                  <Typography.Text>
+                    Пока вы оффлайн нельзя добавить город, подключите интернет и проверьте, возможно
+                    такой город уже есть в базе
+                  </Typography.Text>
+                )}
+              </>
+            )
+          }
+          {...restProps}
+        >
+          {children}
+        </Select>
+        <Modal
+          title="20px to Top"
+          style={{ top: 20 }}
+          open={modal1Open}
+          onOk={() => setModal1Open(false)}
+          onCancel={() => setModal1Open(false)}
+        ></Modal>
+      </>
     );
   }
 );
